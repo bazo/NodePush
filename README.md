3bd3611439b51691e0f41778456c1d8c271ee6c3
Martin Bazik <martin@bazo.sk>
2013-06-28 15:57:08 +0200

    Advanced configuration options

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 NodePush
 ========
 
+## About
 Websocket push server in node.js
 
-install:
+## Installation
+
 npm install
+
+## Configuration
+
+Create config.json file, sample provided in config.sample.json
+```js
+{
+	"server": {
+		"host": "127.0.0.1",
+		"port": 8080
+	}
+}
+```
 
 run:
 
--- server.js
+++ server.js
@@ -1,15 +1,54 @@
-var config = require('./config.json');
-var io = require('socket.io').listen(config.server.port);
+var fs = require('fs');
+var socket = require('socket.io');
+var http = require("http")
+
+var configPath = './config.json';
+
+var defaults = {
+	"server": {
+		"host": "127.0.0.1",
+		"port": 8080
+	},
+	"app": {
+		"debug": false
+	},
+	"io": {
+		"log level": 1
+	}
+};
+
+if(fs.existsSync(configPath)) {
+	var config = require(configPath);
+} else {
+	console.log('config.json missing, using default host and port: ' + defaults.server.host + ':' +defaults.server.port);
+	var config = defaults;
+}
+
+var appOptions = config.hasOwnProperty('app') ? config.app : {};
+
+var server = http.createServer();
+server.listen(config.server.port, config.server.host);
+var io = socket.listen(server, appOptions);
+
+if(config.hasOwnProperty('io')) {
+	for(option in config.io) {
+		io.set(option, config.io.option);
+	}
+}
 
 io.sockets.on('connection', function (socket) {
 
-	socket.on('subscribe', function(data) { 
-		console.log(socket.id + ' joined room: ' + data.room);
+	socket.on('subscribe', function(data) {
+		if(appOptions.debug) {
+			console.log(socket.id + ' joined room: ' + data.room);
+		}
 		socket.join(data.room);
 	});
 
-	socket.on('unsubscribe', function(data) { 
-		console.log(socket.id + ' left room: ' + data.room);
+	socket.on('unsubscribe', function(data) {
+		if(appOptions.debug) {
+			console.log(socket.id + ' left room: ' + data.room);
+		}
 		socket.leave(data.room); 
 	});
 
@@ -19,6 +58,8 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('disconnect', function () {
-		console.log(socket.id + ' disconnected');
+		if(appOptions.debug) {
+			console.log(socket.id + ' disconnected');
+		}
 	});
 });
